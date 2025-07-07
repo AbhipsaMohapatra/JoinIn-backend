d4541ec4de31c36c5e240b88c5d4f47f30fa74ce
Abhipsa Mohapatra <abhipsamohapatra270805@gmail.com>
2025-07-08 00:06:19 +0530

    Added feedback to backend

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const authenticateAdmin = require('../middlewares/auth.js');
+const UserAuthentication = require('../middlewares/userAuth.js');
 
 const run = require('../db.js');
 const e = require('express');
@@ -42,7 +43,7 @@ router.get('/',async(req,res)=>{
     }
 })
 
-router.post('/:id/register',async(req,res)=>{
+router.post('/:id/register',UserAuthentication,async(req,res)=>{
     const event_id = req.params.id;
     const {name,email,phone} = req.body;
     try{
@@ -66,6 +67,86 @@ router.post('/:id/register',async(req,res)=>{
         res.json({error:"Registration Unsuccessful",detail:"Some Error Occured"});
 
     }
+})
+ 
+router.get('/:id/participants',authenticateAdmin,async(req,res)=>{
+    const eventId= req.params.id;
+    try{
+        const con = await run();
+        const[result] = await con.execute('select * from events where id=(?)',[eventId]);
+        if(result.length===0){
+            return res.json({message:"No such event found"})
+        }
+        const[players] = await con.execute('select name,email,phone,registered_at from registrations where event_id=(?) ',[eventId]);
+        res.send({event:result[0].title,players})
+
+
+
+    }
+    catch(e){
+        res.send({error:e.message})
+
+    }
+})
+router.delete('/:id/deleteEvent',authenticateAdmin,async(req,res)=>{
+    const eventId = req.params.id;
+    try{
+        const con = await run();
+        const [events] = await con.execute('select * from events where id=(?)',[eventId]);
+        if(events.length==0){
+            return res.json({message:"No such events present "})
+        }
+        await con.execute('delete from registrations where event_id=(?)',[eventId]);
+        await con.execute('delete from events where id=(?)',[eventId]);
+        res.status(400).json({message:"Event deleated successfully"});
+
+    }
+    catch(e){
+        res.status(404).json({error:e.message});
+
+    }
+})
+router.post('/:id/feedback',UserAuthentication,async (req,res)=>{
+    const eventId = req.params.id;
+    const {rating,comment} = req.body;
+    try{
+        const con = await run();
+        const [event] = await con.execute('select * from events where id=(?)',[eventId]);
+        if(event.length===0){
+            return res.json({message:"No such event present"});
+        }
+        if(!rating || rating<0 || rating>5){
+            return res.json({message:"Invalid rating"})
+        }
+        const [results] = await con.execute('insert into feedback (event_id,rating,comment) values (?,?,?)',[eventId,rating,comment]);
+        res.json({message:"FeedBack Taken Successfully"});
+
+    }
+    catch(e){
+         res.status(500).json({ error: "Failed to submit feedback", detail: e.message });
+
+    }
+});
+router.get('/:id/getFeedback',async(req,res)=>{
+    const eventId = req.params.id;
+    try{
+        const con = await run();
+        const [event] = await con.execute('select * from events where id=(?)',[eventId]);
+        if(event.length===0){
+            return res.json({message:"No such Event exists"})
+
+        }
+        
+        const[results] = await con.execute('select event_id,rating,comment,submitted_at from feedback where event_id=(?)',[eventId]);
+        res.json({event:event[0].title,results});
+
+
+    }
+    catch(e){
+        res.json({error:"Some error occured",message:e.message})
+
+    }
+
 })
 
 module.exports = router;
